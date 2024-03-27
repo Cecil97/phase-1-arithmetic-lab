65ba0be2679875cbb2aae04471d0c0f86e4ba98c
Cecil97 <omondicecil97@gmail.com>
2024-03-27 05:38:48 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1=31;
+let num2=2;
+const multiply= num1*num2 ;
+
+let random ;
+random= Math.floor(Math.random() * 10) + 1;
+
+let num3=32;
+let num4= 8;
+const mod= (num3%num4)+4;
+
+const max=Math.max(1,4,20);
+
+
+
+
+
+
